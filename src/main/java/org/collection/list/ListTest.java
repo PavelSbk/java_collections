126e7fc0c0855a015be4136040ebf715b7a522a5
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-01-18 18:36:26 +0300

    List interface

---
+++ src/main/java/org/collection/list/ListTest.java
@@ -0,0 +1,9 @@
+package org.collection.list;
+
+/**
+ * List - это упорядоченная последовательность элементов, позволяющая хранить дубликаты и null.
+ * Каждый элемент в List имеет index
+ */
+
+public interface ListTest {
+}
